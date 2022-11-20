c1ef1be4847ba90e1391691891bcf188b421ddbe
timmintam <timmintam@hotmail.com>
2022-11-20 12:03:14 +0100

    Update preprocessing

--- task2/pre_processing.py
+++ task2/pre_processing.py
@@ -25,10 +25,6 @@ def ecg(signal, freq=300.0):
     
 def pre_process(signal, freq=300.0):
     ECG_analysis = ecg.ecg(signal=signal, sampling_rate=freq, show=False)
-    ts=ECG_analysis['ts']
-    signal_flt=ECG_analysis['filtered']
-    rpeaks=ECG_analysis['rpeaks']
-    templates_ts=ECG_analysis['templates_ts']
     templates=ECG_analysis['templates']
     
     # check for flipped signals
@@ -36,8 +32,12 @@ def pre_process(signal, freq=300.0):
     templates_max = np.max(np.median(templates, axis=0))
     if np.abs(templates_min) > np.abs(templates_max):
         signal *= -1
-        signal_flt *= -1
-        templates *= -1
+        ECG_analysis = ecg.ecg(signal=signal, sampling_rate=freq, show=False)
+        templates=ECG_analysis['templates']
+    ts=ECG_analysis['ts']
+    signal_flt=ECG_analysis['filtered']
+    rpeaks=ECG_analysis['rpeaks']
+    templates_ts=ECG_analysis['templates_ts']
 
     # normalization
     signal /= templates_max
@@ -45,3 +45,48 @@ def pre_process(signal, freq=300.0):
     templates /= templates_max
     
     return ts, signal, signal_flt, rpeaks, templates_ts, templates
+
+
+def check_R(rpeaks, templates_ts, templates, corr_thresh=0.9, freq=300.0):
+
+    # Check lengths
+    assert len(rpeaks) == len(templates)
+    
+    r_id = int(-templates_ts[0]*freq)
+    median = np.median(templates, axis=0)
+    anom=set()
+    
+    # Loop through rpeaks
+    for i in range(len(templates)):
+        correlation_coefficient = np.corrcoef(median[r_id - 25:r_id + 25],templates[i, r_id - 25:r_id + 25])
+        # Check correlation
+        if correlation_coefficient[0, 1] < corr_thresh:
+            anom.add(i)
+    return anom
+
+"""            # Compute cross correlation
+            cross_correlation = signal.correlate(
+                self.median_template[self.template_rpeak_sp - 25:self.template_rpeak_sp + 25],
+                self.templates[self.template_rpeak_sp - 25:self.template_rpeak_sp + 25, i]
+            )
+
+            # Correct rpeak
+            rpeak_corrected = \
+                self.rpeaks[i] - \
+                (np.argmax(cross_correlation) -
+                    len(self.median_template[self.template_rpeak_sp - 25:self.template_rpeak_sp + 25]))
+
+            # Check to see if shifting the R-Peak improved the correlation coefficient
+            if self.check_improvement(rpeak_corrected, correlation_threshold):
+
+                # Update rpeaks array
+                self.rpeaks[i] = rpeak_corrected
+
+    # Re-extract templates
+    self.templates, self.rpeaks = self.extract_templates(self.rpeaks)
+
+    # Re-compute median template
+    self.median_template = np.median(self.templates, axis=1)
+
+    # Check lengths
+    assert len(self.rpeaks) == self.templates.shape[1]"""
